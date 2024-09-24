291089a29ae8125c495602acd530608b0de2c8ea
Bendegúz Török <bendi.torok@gmail.com>
2024-09-24 22:27:12 +0200

    Add comments

--- libcudaimg/kernel.cu
+++ libcudaimg/kernel.cu
@@ -17,7 +17,7 @@ inline void gpuAssert(cudaError_t code, const char* file, int line, bool abort =
     }
 }
 
-// Invert an image
+// Invert all the pixels in the image
 __global__ void invertImage(unsigned char* image, uint32_t width, uint32_t height) {
     uint32_t x = blockIdx.x * blockDim.x + threadIdx.x;
     uint32_t y = blockIdx.y * blockDim.y + threadIdx.y;
@@ -28,7 +28,13 @@ __global__ void invertImage(unsigned char* image, uint32_t width, uint32_t heigh
     }
 }
 
-// Exposed which calls the invertImage kernel
+/// <summary>
+/// Exposed function which calls the invertImage kernel.
+/// </summary>
+/// <param name="image">Pointer to the first byte of the image.</param>
+/// <param name="image_len">The number of bytes in the image.</param>
+/// <param name="width">The width of the image, should be multiplied by 3 if it's in an RGB format.</param>
+/// <param name="height">The height of the image.</param>
 extern "C" __declspec(dllexport)
 void invertImage(unsigned char* image, uint32_t image_len, uint32_t width, uint32_t height) {
     unsigned char* d_image;
