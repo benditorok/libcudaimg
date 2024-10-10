7b0c5fc0efa8508892abcdf1435ec6b3fbe6c1e4
Bendegúz Török <32034793+benditorok@users.noreply.github.com>
2024-10-10 15:35:16 +0200

    Remove unused code

--- libcudaimg/lib/exports.cuh
+++ libcudaimg/lib/exports.cuh
@@ -77,18 +77,6 @@ namespace exports
 	extern "C" __declspec(dllexport)
 		void boxFilter(unsigned char* image, uint32_t image_len, uint32_t width, uint32_t height, uint32_t filter_size);
 
-	/*/// <summary>
-	/// Exposed function which calls the gaussFilter kernel.
-	/// </summary>
-	/// <param name="image">Pointer to the first byte of the image.</param>
-	/// <param name="image_len">The number of bytes in the image.</param>
-	/// <param name="width">The width of the image, should be multiplied by 3 if it's in an RGB format.</param>
-	/// <param name="height">The height of the image.</param>
-	/// <param name="filter_size">The size of the filter to apply to the image.</param>
-	/// <param name="sigma">The sigma value to apply to the image.</param>
-	extern "C" __declspec(dllexport)
-		void gaussFilter(unsigned char* image, uint32_t image_len, uint32_t width, uint32_t height, float sigma);*/
-
 	/// <summary>
 	/// Exposed function which calls the gaussianBlur kernel.
 	/// </summary>
